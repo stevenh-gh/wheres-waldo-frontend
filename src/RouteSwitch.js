6c0ba1dfdfeecc0e86912d30a02b66fcb2a1f258
steven <steven.h@live.com>
2022-03-09 14:14:48 -0800

    add tailwind styling to body when page loads

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 
 const RouteSwitch = () => {
+    useEffect(() => {
+        document.body.classList.add(...["container", "m-auto"]);
+    }, []);
+
     return (
         <BrowserRouter>
             <Routes>
