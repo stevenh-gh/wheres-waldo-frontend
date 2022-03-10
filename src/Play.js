cd4bc9c8c1828cfd0fbaa440b06cf2bf50d67464
steven <steven.h@live.com>
2022-03-09 17:56:55 -0800

    add hoverable grids to image

--- src/Play.js
+++ src/Play.js
@@ -2,7 +2,16 @@ const Play = () => {
     return (
         <>
             <div>the game page</div>
-            <div className="bg-[url('../public/waldo.jpeg')] w-[1250px] h-[772px] bg-contain bg-no-repeat"></div>
+            <div className="bg-[url('../public/waldo.jpeg')] w-[1175px] h-[726px] bg-contain bg-no-repeat grid grid-cols-[repeat(25,_1fr)]">
+                {[...Array(625)].map((el, index) => {
+                    return (
+                        <div
+                            className="hover:border hover:border-blue-500"
+                            key={index}
+                        ></div>
+                    );
+                })}
+            </div>
         </>
     );
 };
