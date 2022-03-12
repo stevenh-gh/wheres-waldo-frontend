d24216c6d8b25b3e10a68277585a1f51c19eddfb
steven <steven.h@live.com>
2022-03-11 16:19:26 -0800

    add link to play

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 const App = () => {
-    return <div className="text-4xl text-center">hello world!</div>;
+    return (
+        <>
+            <h2>Choose a level</h2>
+            <Link to="/play">
+                <div className="bg-[url('../public/waldo.jpeg')] w-[900px] h-[556px] bg-contain bg-no-repeat hover:scale-105"></div>
+            </Link>
+        </>
+    );
 };
 
 export default App;
