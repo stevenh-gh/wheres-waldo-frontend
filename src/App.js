136be55955ebbc57a2ee80d956fea34ec1da7a35
steven <steven.h@live.com>
2022-03-14 01:34:04 -0700

    call fetch to api and display image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const App = () => {
+    const [jsonData, setJsonData] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/api/games")
+            .then((resp) => {
+                return resp.json();
+            })
+            .then((resp) => setJsonData(resp))
+            .catch((resp) => console.log(resp));
+    }, []);
+
     return (
         <>
             <h2>Choose a level</h2>
-            <Link to="/play">
+            {jsonData.map((ele, idx) => {
+                return <img key={idx} src={ele.img} alt="" />;
+            })}
+            {/* <Link to="/play">
                 <div className="bg-[url('../public/waldo.jpeg')] w-[900px] h-[556px] bg-contain bg-no-repeat hover:scale-105"></div>
-            </Link>
+            </Link> */}
         </>
     );
 };
